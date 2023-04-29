e4e684fe826b2e98e17c5d5dd6b4970c94ebad83
28Aryan <aryanrc28@gmail.com>
2023-04-30 02:52:52 +0530

    Mes commit

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -2,9 +2,13 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -35,7 +39,6 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        auth=FirebaseAuth.getInstance();
         signup_email=findViewById(R.id.signup_email);
         signup_password=findViewById(R.id.signup_password);
         signup_btn=findViewById(R.id.signup_btn);
@@ -47,6 +50,14 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View v) {
                 String email=signup_email.getText().toString().trim();
                 String pass=signup_password.getText().toString().trim();
+
+                auth=FirebaseAuth.getInstance();
+
+                if(ContextCompat.checkSelfPermission(SignUp.this, android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+                    sendMessaage();
+                }else{
+                    ActivityCompat.requestPermissions(SignUp.this, new String[]{android.Manifest.permission.SEND_SMS},100);
+                }
 
                 if (email.isEmpty()){
                     signup_email.setError("Email Cannot Be Empty");
@@ -90,8 +101,22 @@ public class SignUp extends AppCompatActivity {
                 startActivity(new Intent(SignUp.this,Login.class));
             }
         });
-    }
 
+    }
+    public void sendMessaage(){
+        String num = signup_phn.getText().toString().trim();
+        String mes = "Please send NGO registration on Whatsapp on the below provided number." +
+                " \n 9769312232";
+
+        if(!num.equals("") && !mes.equals("")){
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(num,null,mes,null,null);
+            Toast.makeText(getApplicationContext(), "SMS sent successfully!", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Enter number", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     public void onBackPressed(){
         super.onBackPressed();

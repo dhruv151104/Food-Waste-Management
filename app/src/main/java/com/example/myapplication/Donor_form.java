b40d5228ff27332b792573b65838447bf56a4d01
dhruv151104 <85018528+dhruv151104@users.noreply.github.com>
2023-04-29 14:21:43 +0530

    made changes in upload detais

--- app/src/main/java/com/example/myapplication/Donor_form.java
+++ app/src/main/java/com/example/myapplication/Donor_form.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.;
+package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Upload_Details extends AppCompatActivity {
+public class Donor_form extends AppCompatActivity {
     Button btnsubmit,contactngo;
     EditText inputUsername,inputContact,inputfoodtype,inputfoodquantity,inputcity,inputother;
     DatabaseReference databaseUsers;
@@ -38,40 +38,40 @@ public class Upload_Details extends AppCompatActivity {
         contactngo.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Upload_Details.this,NGO.class));
+              //  startActivity(new Intent(Upload_Details.this,NGO.class));
             }
         });
 
         btnsubmit.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                InsertData();
+                //InsertData();
             }
         });
     }
 
-    public void InsertData() {
-        String name=inputUsername.getText().toString();
-        String contact=inputContact.getText().toString();
-        String food=inputfoodtype.getText().toString();
-        String quantity=inputfoodquantity.getText().toString();
-        String city=inputcity.getText().toString();
-        String other=inputother.getText().toString();
-        String id = databaseUsers.push().getKey();
-
-        details user= new details(name,contact,food,quantity,city,other);
-        databaseUsers.child("users").child(id).setValue(user)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Upload_Details.this, "Food Details Inserted", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-
-
-
-    }
+//    public void InsertData() {
+//        String name=inputUsername.getText().toString();
+//        String contact=inputContact.getText().toString();
+//        String food=inputfoodtype.getText().toString();
+//        String quantity=inputfoodquantity.getText().toString();
+//        String city=inputcity.getText().toString();
+//        String other=inputother.getText().toString();
+//        String id = databaseUsers.push().getKey();
+//
+//        details user= new details(name,contact,food,quantity,city,other);
+//        databaseUsers.child("users").child(id).setValue(user)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if(task.isSuccessful()){
+//                            Toast.makeText(Upload_Details.this, "Food Details Inserted", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//
+//
+//
+//
+//    }
 }

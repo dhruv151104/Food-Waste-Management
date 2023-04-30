6e04e69c3323bc971a5852ecd71f97712617c030
dhruv151104 <85018528+dhruv151104@users.noreply.github.com>
2023-04-30 08:18:57 +0530

    done with view dwtails

--- app/src/main/java/com/example/myapplication/user_option.java
+++ app/src/main/java/com/example/myapplication/user_option.java
@@ -25,7 +25,7 @@ public class user_option extends AppCompatActivity {
         Ngo_contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(user_option.this,null));
+                startActivity(new Intent(user_option.this,ngo_table.class));
             }
         });
     }
